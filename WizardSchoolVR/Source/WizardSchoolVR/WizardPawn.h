977463b5231a39d5dec0d4c73b3d28bd384994b2
chrisfields0 <16chrisfields16@gmail.com>
2017-10-04 10:35:22 -0700

    added in grabbing code so you get the closest object to you

--- WizardSchoolVR/Source/WizardSchoolVR/WizardPawn.h
+++ WizardSchoolVR/Source/WizardSchoolVR/WizardPawn.h
@@ -6,7 +6,13 @@
 #include "GameFramework/Pawn.h"
 #include "MotionControllerComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
 #include "WizardPawn.generated.h"
+
+class AWizardPawn;
+class AInteractableItem;
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnDropItem, AWizardPawn* const, int);
 
 UCLASS()
 class WIZARDSCHOOLVR_API AWizardPawn : public APawn
@@ -28,7 +34,22 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+/*
+ * Grab Interaction System Interface
+ */
+public:
+	UFUNCTION(BlueprintCallable, Category = "Grab Interaction System")
+	void UpdateClosestInteractableToHand(USphereComponent* InteractionSphere, bool bForLeftController);
 
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Grab Iteraction System")
+	AInteractableItem* GetLeftClosestInteractableItem() const { return L_ClosestInteractableItem.Get(); }
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Grab Iteraction System")
+	AInteractableItem* GetRightlosestInteractableItem() const { return R_ClosestInteractableItem.Get(); }
+	FOnDropItem& OnDropItemDelegate() { return OnDropItem; }
+protected:
+	TWeakObjectPtr<AInteractableItem> L_ClosestInteractableItem;
+	TWeakObjectPtr<AInteractableItem> R_ClosestInteractableItem;
+	FOnDropItem OnDropItem;
 /*
  *	HMD Character Interface
  */
@@ -50,13 +71,16 @@ protected:
 	class UMotionControllerComponent* L_MotionController;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	class USkeletalMeshComponent* L_HandMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* L_InteractionBubble;
 
 	/** Motion controller (right hand) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	class UMotionControllerComponent* R_MotionController;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	class USkeletalMeshComponent* R_HandMesh;
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* R_InteractionBubble;
 private:
 	void GrabLeftPressed();
 	void GrabLeftReleased();
